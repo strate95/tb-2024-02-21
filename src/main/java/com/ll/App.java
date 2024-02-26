b7dc5b075579b84547a28dd86370b87e9a4e1844
strate95 <wfpjhw95@gmail.com>
2024-02-26 21:37:53 +0900

    step 5 list

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,5 +1,7 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 class App {
@@ -10,6 +12,7 @@ class App {
         System.out.print("명령) ");
         String order = scanner.nextLine();
         int goodsaynum = 1;
+        List<WSlist> WSlists = new ArrayList<>();
         while(true) {
             if (order.equals("종료")) {
                 break;
@@ -21,7 +24,22 @@ class App {
                 String author = scanner.nextLine();
                 System.out.println("명언) " + contents);
                 System.out.println("작가) " + author);
-                System.out.println(goodsaynum++ +"번 명언이 등록되었습니다.");
+                WSlist WS = new WSlist(goodsaynum, contents, author);
+                WSlists.add(WS);
+                System.out.println(goodsaynum++ + "번 명언이 등록되었습니다.");
+            }
+            else if (order.equals("목록")){
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("---------------------------------------");
+                if (WSlists.isEmpty()){
+                    System.out.println("등록된 명언이 없습니다.");
+                }
+                else {
+                    for (int i = WSlists.size() - 1; i >= 0; i--) {
+                        WSlist WS = WSlists.get(i);
+                        System.out.println(WS.num + " / " + WS.content + " / " + WS.author);
+                    }
+                }
             }
             else {
                 System.out.println("입력받은 명령) " + order + "(은)는 존재하지 않는 명령입니다.");
@@ -32,3 +50,4 @@ class App {
         }
     }
 }
+
---
+++ src/main/java/com/ll/WSlist.java
@@ -0,0 +1,13 @@
+package com.ll;
+
+public class WSlist {
+    int num;
+    String content;
+    String author;
+
+    public WSlist(int num, String content, String author) {
+        this.num = num;
+        this.content = content;
+        this.author = author;
+    }
+}
