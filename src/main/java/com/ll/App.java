cca61b06cd242ba597c667a7c7df7d4a14e1755b
strate95 <wfpjhw95@gmail.com>
2024-03-06 20:29:55 +0900

    Rq 도입 및 삭제처리완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -8,36 +8,42 @@ class App {
     Scanner scanner;
     int goodsaynum;
     List<WSlist> WSlists;
-    App(){
+
+    App() {
         scanner = new Scanner(System.in);
         goodsaynum = 1;
         WSlists = new ArrayList<>();
     }
+
     void run() {
         System.out.println("프로그램 실행");
         System.out.println("== 명언 앱 ==");
-        while(true) {
+        while (true) {
             System.out.print("명령) ");
             String order = scanner.nextLine();
-            if (order.equals("종료")) {
-                break;
-            }
-            else if (order.equals("등록")){
-                orderwrite();
-            }
-            else if (order.equals("목록")){
-                orderlist();
-            }
-            else if (order.startsWith("삭제")){
-                orderdelete(order);
-            }
-            else {
-                System.out.println("입력받은 명령) " + order + "(은)는 존재하지 않는 명령입니다.");
-                System.out.println("다시 명령을 내려주세요.");
+            Rq rq = new Rq(order);
+            System.out.println("입력받은 명령 : " + rq.getAction());
+            System.out.println("입력받은 id : " + rq.getParamAsInt("id", 0));
+            switch (rq.getAction()){
+                case "종료" :
+                    return ;
+                case "등록" :
+                    orderwrite();
+                    break;
+                case "목록" :
+                    orderlist();
+                    break;
+                case "삭제" :
+                    orderdelete(rq);
+                    break;
+                case "수정" :
+                    ordermodify(rq);
+                    break;
             }
         }
     }
-    void orderwrite(){
+
+    void orderwrite() {
         System.out.print("명언 : ");
         String contents = scanner.nextLine();
         System.out.print("작가 : ");
@@ -48,34 +54,49 @@ class App {
         WSlists.add(WS);
         System.out.println(goodsaynum++ + "번 명언이 등록되었습니다.");
     }
-    void orderlist(){
+
+    void orderlist() {
         System.out.println("번호 / 작가 / 명언");
         System.out.println("---------------------------------------");
-        if (WSlists.isEmpty()){
+        if (WSlists.isEmpty()) {
             System.out.println("등록된 명언이 없습니다.");
-        }
-        else {
+        } else {
             for (int i = WSlists.size() - 1; i >= 0; i--) {
                 WSlist WS = WSlists.get(i);
                 System.out.println(WS.num + " / " + WS.content + " / " + WS.author);
             }
         }
     }
-    void orderdelete(String order){
-        String neworder = order.replace("삭제?id=", "");
-        int removenum = Integer.parseInt(neworder);
-        int count = 0;
+
+    int getIndexofgoodsayingbyid(int id){
         for(int i = 0 ; i < WSlists.size() ; i++){
             WSlist ws = WSlists.get(i);
-            if (removenum == ws.num){
-                WSlists.remove(i);
-                System.out.println("명언이 삭제되었습니다");
-                count++;
+            if (ws.num == id){
+                return i;
             }
         }
-        if (count == 0){
-            System.out.println("입력하신 번호의 명언이 없습니다.");
+        return -1;
+    }
+
+    void orderdelete(Rq rq) {
+        int removeid = rq.getParamAsInt("id", 0);
+        int index = getIndexofgoodsayingbyid(removeid);
+        if (index == -1) {
+            System.out.println("입력하신 번호의 명언이 없습니다. 정확한 번호를 입력해 주세요");
+        }
+        else{
+            WSlists.remove(index);
+            System.out.println(removeid + "번 명언이 삭제되었습니다");
+        }
+    }
+    void ordermodify(Rq rq){
+        int modifyid = rq.getParamAsInt("id", 0);
+        int index = getIndexofgoodsayingbyid(modifyid);
+        if (index == -1){
+            System.out.println("입력하신 번호의 명언이 없습니다. 정확한 번호를 입력해 주세요");
+        }
+        else{
+
         }
     }
 }
-
---
+++ src/main/java/com/ll/Rq.java
@@ -0,0 +1,51 @@
+package com.ll;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Rq {
+    String order;
+    String action;
+    String queryString;
+    Map<String, String> paramsMap;
+    Rq(String order) {
+        paramsMap = new HashMap<>();
+
+        this.order = order;
+
+        String[] orderBits = order.split("\\?", 2);
+        action = orderBits[0].trim();
+        if(orderBits.length == 1) {
+            return;
+        }
+        queryString = orderBits[1].trim();
+
+        String[] queryStringBits = queryString.split("&");
+
+        for (int i = 0; i < queryStringBits.length; i++) {
+            String queryParamStr = queryStringBits[i];
+            String[] queryParamStrBits = queryParamStr.split("=", 2);
+
+            String paramName = queryParamStrBits[0];
+            String paramValue = queryParamStrBits[1];
+            paramsMap.put(paramName, paramValue);
+        }
+    }
+
+    String getAction() {
+        return action;
+    }
+
+    public int getParamAsInt(String paramName, int defaultValue) {
+        String paramValue = paramsMap.get(paramName);
+        if(paramValue != null){
+            try{
+                return Integer.parseInt(paramValue);
+            }
+            catch (NumberFormatException e){
+
+            }
+        }
+        return defaultValue;
+    }
+}
